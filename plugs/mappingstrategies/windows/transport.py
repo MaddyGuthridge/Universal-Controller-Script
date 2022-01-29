66a203e689d00aea112e3bd1ec5fb32d88329d01
Miguel Guthridge <hdsq@outlook.com.au>
2022-01-29 17:25:55 +1100

    Improve callback binding in transport plugin

--- plugs/mappingstrategies/windows/transport.py
+++ plugs/mappingstrategies/windows/transport.py
@@ -4,7 +4,14 @@ import ui
 
 from typing import Any
 from common.util.apifixes import PluginIndex
-from controlsurfaces import ControlShadow, JogForwards, JogBackards, JogWheel
+from controlsurfaces import (
+    ControlShadow,
+    PlayButton,
+    StopButton,
+    JogForwards,
+    JogBackards,
+    JogWheel
+)
 from devices import DeviceShadow
 from plugs import Plugin
 
@@ -12,15 +19,21 @@ class Transport(Plugin):
     
     def __init__(self, shadow: DeviceShadow) -> None:
         super().__init__(shadow, [])
+        shadow.bindFirstMatchSafe(PlayButton, self.playButton)
+        shadow.bindFirstMatchSafe(StopButton, self.stopButton)
+        shadow.bindFirstMatchSafe(JogWheel, self.jogWheel)
         
-    def playButton(self, control: ControlShadow, index: PluginIndex, *args: Any):
+    def playButton(self, control: ControlShadow, index: PluginIndex, *args: Any) -> bool:
         transport.start()
+        return True
 
-    def stopButton(self, control: ControlShadow, index: PluginIndex, *args: Any):
+    def stopButton(self, control: ControlShadow, index: PluginIndex, *args: Any) -> bool:
         transport.stop()
+        return True
 
-    def jogWheel(self, control: ControlShadow, index: PluginIndex, *args: Any):
+    def jogWheel(self, control: ControlShadow, index: PluginIndex, *args: Any) -> bool:
         
         increment = 1 if isinstance(control.getControl(), JogForwards) else -1
         
         ui.jog(increment)
+        return True
