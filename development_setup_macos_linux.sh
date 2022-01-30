26fca0ec34e000849fa35cfbd8fc819837cba175
Miguel Guthridge <hdsq@outlook.com.au>
2022-01-30 19:46:15 +1100

    Create development setup scripts

---
+++ development_setup_macos_linux.sh
@@ -0,0 +1,4 @@
+
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip3 install -r requirements.txt
---
+++ development_setup_windows.ps1
@@ -0,0 +1,4 @@
+
+python -m venv .venv
+. .\.venv\Scripts\Activate.ps1
+pip install -r requirements.txt
