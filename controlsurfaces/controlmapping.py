7b25f75c2d13772998ed6cf9cc78554d3e0613fc
Miguel Guthridge <hdsq@outlook.com.au>
2022-01-31 19:05:38 +1100

    Fix an issue where control mappings wouldn't be equated correctly

--- controlsurfaces/controlmapping.py
+++ controlsurfaces/controlmapping.py
@@ -15,9 +15,21 @@ if TYPE_CHECKING:
     from . import ControlSurface
 
 class ControlMapping:
-    
+    """
+    Defines a mapping to a control surface, which has the property that
+    different instances of a mapping to the same control have the same hash.
+    """
     def __init__(self, mapping: 'ControlSurface') -> None:
         self.__mapping = mapping
+    
+    def __hash__(self) -> int:
+        return hash(self.__mapping)
+    
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, ControlMapping):
+            return self.__mapping == __o.__mapping
+        else:
+            return False
     
     def getControl(self) -> 'ControlSurface':
         return self.__mapping
