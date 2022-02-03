f563ce14e36126b601c5fff7c3026c269b232447
Miguel Guthridge <hdsq@outlook.com.au>
2022-02-03 11:51:19 +1100

    Fix implementation of pitch bend

--- controlsurfaces/wheels.py
+++ controlsurfaces/wheels.py
@@ -1,7 +1,8 @@
 
 from common.eventpattern import BasicEventPattern, fromNibbles
+from common.types import eventData
 from . import ControlSurface
-from . import Data2Strategy
+from . import Data2Strategy, IValueStrategy
 
 class ModWheel(ControlSurface):
     """
@@ -13,10 +14,25 @@ class ModWheel(ControlSurface):
     
     def __init__(self) -> None:
         super().__init__(
-            BasicEventPattern(0xB0, 0x1, ...),
+            BasicEventPattern(fromNibbles(0xB, ...), 0x1, ...),
             Data2Strategy(),
             "transport"
             )
+
+class PitchValueStrategy(IValueStrategy):
+    """Value strategy for pitch bends
+    """
+    def getValueFromEvent(self, event: eventData) -> int:
+        """Returns a 14-bit int (0 - 16384)
+        Zero value = 8192
+        """
+        return event.data1 + (event.data2 << 7)
+    
+    def getFloatFromValue(self, value: int) -> float:
+        return value / 16384
+    
+    def getValueFromFloat(self, f: float) -> int:
+        return int(f * 16384)
 
 class PitchWheel(ControlSurface):
     """
@@ -28,7 +44,7 @@ class PitchWheel(ControlSurface):
     
     def __init__(self) -> None:
         super().__init__(
-            BasicEventPattern(fromNibbles(0xB, ...), 0x1, ...),
-            Data2Strategy(),
+            BasicEventPattern(fromNibbles(0xE, ...), 0x1, ...),
+            PitchValueStrategy(),
             "transport"
             )
