5453f581ade669f410870279bf8582bab63b5591
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-03 18:25:15 +1100

    Add tests for eventdata

---
+++ tests/test_forward.py
@@ -0,0 +1,22 @@
+"""
+tests > test_forward
+
+Tests for forwarded messages, including encoding and decoding
+
+Authors:
+* Miguel Guthridge
+"""
+
+from common.types import EventData
+from common.util.events import (
+    encodeForwardedEvent,
+    decodeForwardedEvent
+)
+
+def test_encode_decode():
+    """When an event is encoded then decoded, is it still equivalent?"""
+    e = EventData(1, 2, 3)
+    assert e == decodeForwardedEvent(EventData(encodeForwardedEvent(e, 1)))
+    
+    e = EventData([5, 2, 7, 4, 1])
+    assert e == decodeForwardedEvent(EventData(encodeForwardedEvent(e, 1)))
