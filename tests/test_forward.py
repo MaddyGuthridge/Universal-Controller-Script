90f8d872e0df6ffab98d9ca6d7f623894ac22501
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-03 19:42:49 +1100

    Add some more event tests

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -10,7 +10,8 @@ Authors:
 from common.types import EventData
 from common.util.events import (
     encodeForwardedEvent,
-    decodeForwardedEvent
+    decodeForwardedEvent,
+    isEventForwarded
 )
 
 def test_encode_decode():
@@ -20,3 +21,7 @@ def test_encode_decode():
     
     e = EventData([5, 2, 7, 4, 1])
     assert e == decodeForwardedEvent(EventData(encodeForwardedEvent(e, 1)))
+
+def test_isEventForwarded():
+    """Make sure the forwarded event type checker is working"""
+    assert isEventForwarded(EventData(encodeForwardedEvent(EventData(1, 2, 3), 1)))
