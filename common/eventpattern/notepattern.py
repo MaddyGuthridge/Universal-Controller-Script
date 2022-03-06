44279bbb3418d05e4b3d29bdf80c0ded35e56d87
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-06 16:12:31 +1100

    Make NotePattern support specified channels

--- common/eventpattern/notepattern.py
+++ common/eventpattern/notepattern.py
@@ -8,17 +8,17 @@ Authors:
 """
 
 from . import BasicPattern
-from .bytematch import fromNibbles
+from .bytematch import fromNibbles, ByteMatch
 
 class NotePattern(BasicPattern):
     """
     The note pattern is used as a way to recognise note events
     """
-    def __init__(self, note_num: int) -> None:
+    def __init__(self, note_num: int, channel_match: ByteMatch = ...) -> None:
         """
         Create a NotePattern
 
         ### Args:
         * `note_num` (`int`): note number to recognise
         """
-        super().__init__(fromNibbles((8, 9), ...), note_num, ...)
+        super().__init__(fromNibbles((8, 9), channel_match), note_num, ...)
