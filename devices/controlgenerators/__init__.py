efad8d05cccce5cd66239b313b67cd527b6f34f4
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-14 15:29:12 +1100

    Add docstrings for control generators

--- devices/controlgenerators/__init__.py
+++ devices/controlgenerators/__init__.py
@@ -3,6 +3,9 @@ devices > controlgenerators
 
 Contains definitions for helper functions for creating and binding common
 controls quickly
+
+Authors:
+* Miguel Guthridge [hdsq@outlook.com.au, HDSQ#2154]
 """
 
 from .notes import NoteMatcher, NoteAfterTouchMatcher
