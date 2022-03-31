99dde94b2491fecac8746b0bfe7751b3e27f4ac7
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-31 21:49:08 +1100

    Add tests for devices

---
+++ tests/test_devices.py
@@ -0,0 +1,22 @@
+"""
+tests > test_devices
+
+Test to ensure device objects are behaving correctly
+"""
+
+from common import ExtensionManager
+
+def test_create():
+    for dev in ExtensionManager.getAllDevices():
+        dev.create(None)
+
+def test_getId():
+    for dev in ExtensionManager.getAllDevices():
+        d = dev.create(None)
+        # It should have dots in it somewhere
+        assert "." in d.getId()
+
+def test_getUniversalEnquiryResponsePattern():
+    for dev in ExtensionManager.getAllDevices():
+        d = dev.create(None)
+        d.getUniversalEnquiryResponsePattern()
