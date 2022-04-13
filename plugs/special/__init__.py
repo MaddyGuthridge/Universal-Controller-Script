eaa39922a99d0804d71b701fec19ed54d67e42aa
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-13 16:16:33 +1000

    Improve documentation for special plugs

--- plugs/special/__init__.py
+++ plugs/special/__init__.py
@@ -1,8 +1,16 @@
+"""
+plugs > special
+
+Contains the definitions for special plugins, which are used to manage global
+behaviours such as transport, macros, and fallback processing.
+"""
 
 __all__ = [
     'transport',
     'fallback',
+    'macro'
 ]
 
 from . import transport
 from . import fallback
+from . import macro
