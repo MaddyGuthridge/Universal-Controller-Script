c4794e437320a86fefb6bf67e84d6520e318dfc4
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-18 03:51:27 +1000

    Add idea for future version

--- controlsurfaces/controlshadow.py
+++ controlsurfaces/controlshadow.py
@@ -128,9 +128,10 @@ class ControlShadow:
           black as transparent
         """
         # If our device shadow is transparent, we should only set the colour
-
         if transparent:
             if self.color != Color():
+                # IDEA: Superimpose the added colour
+                # Requires smarter updating of plugins and stuff
                 self._control.color = self.color
                 self._changed = False
         # If we're being thorough, or the shadow has changed since last time,
