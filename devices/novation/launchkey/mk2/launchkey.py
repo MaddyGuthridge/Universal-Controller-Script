19ed9e69e9871bb88f5facef5bce348bf2f4cf4b
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-17 21:34:07 +1000

    Add control switch and metronome buttons to Launchkey

--- devices/novation/launchkey/mk2/launchkey.py
+++ devices/novation/launchkey/mk2/launchkey.py
@@ -11,7 +11,7 @@ from typing import Optional
 
 import device
 
-from common.eventpattern import BasicPattern, ForwardedPattern
+from common.eventpattern import BasicPattern, ForwardedPattern, NotePattern
 from common.extensionmanager import ExtensionManager
 from common.types import EventData
 from controlsurfaces import (
@@ -30,11 +30,14 @@ from controlsurfaces import (
     StandardModWheel,
     StandardPitchWheel,
     StopButton,
+    ControlSwitchButton,
+    MetronomeButton,
 )
 from controlsurfaces.valuestrategies import (
     ButtonData2Strategy,
     Data2Strategy,
     ForwardedStrategy,
+    NoteStrategy,
 )
 from devices import BasicControlMatcher, Device
 from devices.controlgenerators import NoteMatcher
@@ -62,6 +65,20 @@ class LaunchkeyMk2(Device):
         for r in range(self.getDrumPadSize()[0]):
             for c in range(self.getDrumPadSize()[1]):
                 matcher.addControl(LaunchkeyDrumpad((r, c)), 10)
+
+        # Control switch and metronome buttons
+        matcher.addControl(
+            ControlSwitchButton(
+                ForwardedPattern(2, NotePattern(0x68, 0xF)),
+                ForwardedStrategy(NoteStrategy()),
+            )
+        )
+        matcher.addControl(
+            MetronomeButton(
+                ForwardedPattern(2, NotePattern(0x78, 0xF)),
+                ForwardedStrategy(NoteStrategy()),
+            )
+        )
 
         # Create knobs
         for i in range(8):
