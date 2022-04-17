5c2ea19017d324eb96b0549f1ee5bb1d104374b4
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-17 21:11:45 +1000

    Add pause active and control switch macro buttons

--- controlsurfaces/macrobutton.py
+++ controlsurfaces/macrobutton.py
@@ -32,14 +32,14 @@ class MacroButton(Button):
     """
 
 
-class SaveButton(Button):
+class SaveButton(MacroButton):
     """
     Defines a save button, which will be mapped to the save command in FL
     Studio
     """
 
 
-class UndoRedoButton(Button):
+class UndoRedoButton(MacroButton):
     """
     Defines an undo-redo button, which will be mapped to FL Studio's undo-redo
     command.
@@ -49,7 +49,7 @@ class UndoRedoButton(Button):
     """
 
 
-class UndoButton(Button):
+class UndoButton(MacroButton):
     """
     Defines an undo button, which will be mapped to FL Studio's undo command.
 
@@ -57,7 +57,7 @@ class UndoButton(Button):
     """
 
 
-class RedoButton(Button):
+class RedoButton(MacroButton):
     """
     Defines a redo button, which will be mapped to FL Studio's redo command.
 
@@ -65,14 +65,14 @@ class RedoButton(Button):
     """
 
 
-class QuantizeButton(Button):
+class QuantizeButton(MacroButton):
     """
     Defines a quantize button, which should be mapped to FL Studio's snapping
     control.
     """
 
 
-class SwitchActiveButton(Button):
+class SwitchActiveButton(MacroButton):
     """
     Defines a switch active button, which is handled internally to switch
     event handling between the active plugin and the active window.
@@ -99,20 +99,36 @@ class SwitchActiveButton(Button):
 
 class SwitchActivePluginButton(SwitchActiveButton):
     """
-    A switch active button that make generator and effects plugins be actively
-    processed by the script
+    A switch active button that makes generator and effects plugins be actively
+    processed by the script.
     """
 
 
 class SwitchActiveWindowButton(SwitchActiveButton):
     """
-    A switch active button that make FL Studio windows be actively
-    processed by the script
+    A switch active button that makes FL Studio windows be actively
+    processed by the script.
     """
 
 
 class SwitchActiveToggleButton(SwitchActiveButton):
     """
     A switch active button that toggles between plugins and FL Studio windows
-    being actively processed by the script
+    being actively processed by the script.
+    """
+
+
+class PauseActivePluginButton(MacroButton):
+    """
+    A pause active button that pauses the updating of the active plugin,
+    allowing users to keep their parameters mapped to the currently selected
+    plugin, even if they choose a new plugin.
+    """
+
+
+class ControlSwitchButton(MacroButton):
+    """
+    A button that is used to switch modes in some plugins (for example when
+    changing between views). In order to get full control of the script,
+    devices should implement this button somewhere.
     """
