20e879d6c23dc5b170158558b1427d39039c0162
Miguel Guthridge <hdsq@outlook.com.au>
2022-05-17 20:22:12 +1000

    Test mypy

--- src/devices/deviceshadow.py
+++ src/devices/deviceshadow.py
@@ -52,9 +52,7 @@ StandardEventCallback = Union[
 EventCallback = StandardEventCallback
 
 if TYPE_CHECKING:
-    ArgGenerator: TypeAlias = list[tuple] \
-        | Callable[[list[ControlShadow]], Generator[tuple, None, None]] \
-        | ellipsis | None  # noqa: F821
+    ArgGenerator: TypeAlias = list[tuple] | Callable[[list[ControlShadow]], Generator[tuple, None, None]] | ellipsis | None  # noqa: F821
 else:
     ArgGenerator: TypeAlias = type
 
