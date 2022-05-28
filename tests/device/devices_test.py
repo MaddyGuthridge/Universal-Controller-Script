07bb363e5874e5c321800ed12789aaeb42e203ac
Miguel Guthridge <hdsq@outlook.com.au>
2022-05-28 17:01:35 +1000

    Fix pytest

--- tests/device/devices_test.py
+++ tests/device/devices_test.py
@@ -6,24 +6,25 @@ Test to ensure device objects are behaving correctly
 
 import pytest
 from common import ExtensionManager
+from devices import Device
 
 
 @pytest.mark.parametrize(
     'dev',
     ExtensionManager.devices.all()
 )
-def test_create(dev):
-    dev.create(None)
+def test_create_from_id(dev: Device):
+    for id in dev.getSupportedIds():
+        dev.create(id=id)
 
 
-def test_getId():
-    for dev in ExtensionManager.devices.all():
-        d = dev.create(None)
-        # It should have dots in it somewhere
-        assert "." in d.getId()
-
-
-def test_getUniversalEnquiryResponsePattern():
-    for dev in ExtensionManager.devices.all():
-        d = dev.create(None)
-        d.getUniversalEnquiryResponsePattern()
+@pytest.mark.parametrize(
+    'dev',
+    ExtensionManager.devices.all()
+)
+def test_create_from_response(dev: Device):
+    pattern = dev.getUniversalEnquiryResponsePattern()
+    if pattern is not None:
+        dev.create(pattern.fulfil())
+    else:
+        dev.create(None)
