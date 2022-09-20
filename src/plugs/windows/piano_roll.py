ff086224a5beb2398255b3d9db643052bdf2410c
Miguel Guthridge <hdsq@outlook.com.au>
2022-09-20 23:55:42 +1000

    Add support for tool selection to piano roll

--- src/plugs/windows/piano_roll.py
+++ src/plugs/windows/piano_roll.py
@@ -9,11 +9,29 @@ Authors:
 This code is licensed under the GPL v3 license. Refer to the LICENSE file for
 more details.
 """
+import transport
+import ui
 from common.extension_manager import ExtensionManager
+from common.types import Color
 from devices import DeviceShadow
 from plugs import WindowPlugin
+from plugs.event_filters import filterButtonLift
+from control_surfaces import ToolSelector, ControlShadowEvent
 
 INDEX = 3
+
+
+COLORS = [
+    Color.fromInteger(0xffc43f),  # Pencil
+    Color.fromInteger(0x7bcefd),  # Paint
+    Color.fromInteger(0xad90fe),  # Paint drums
+    Color.fromInteger(0xfe5750),  # Delete
+    Color.fromInteger(0xff54b0),  # Mute
+    Color.fromInteger(0x85b3f2),  # Slice
+    Color.fromInteger(0xff9354),  # Select
+    Color.fromInteger(0x80acff),  # Zoom
+    Color.fromInteger(0xffa64a),  # Preview
+]
 
 
 class PianoRoll(WindowPlugin):
@@ -24,6 +42,8 @@ class PianoRoll(WindowPlugin):
     """
 
     def __init__(self, shadow: DeviceShadow) -> None:
+        shadow.bindMatches(ToolSelector, self.eSelectTool, args_generator=...)\
+            .colorize(COLORS)
         super().__init__(shadow, [])
 
     @classmethod
@@ -33,6 +53,27 @@ class PianoRoll(WindowPlugin):
     @classmethod
     def create(cls, shadow: DeviceShadow) -> 'WindowPlugin':
         return cls(shadow)
+
+    @filterButtonLift()
+    def eSelectTool(
+        self,
+        control: ControlShadowEvent,
+        window,
+        idx: int,
+    ) -> bool:
+        # FIXME: This uses keyboard shortcuts which are extremely unreliable
+        if idx < 9:
+            # Open menu
+            transport.globalTransport(90, 1)
+            # Navigate to tool selection
+            ui.left()
+            ui.left()
+            # Move down until we reach the required tool
+            for _ in range(idx):
+                ui.down()
+            # Select it
+            ui.enter()
+        return True
 
 
 ExtensionManager.windows.register(PianoRoll)
