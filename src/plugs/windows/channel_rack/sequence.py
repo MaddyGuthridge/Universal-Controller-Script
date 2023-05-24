2732fc3b5905538950381e593dd2576b459f3e2d
Miguel Guthridge <hdsq@outlook.com.au>
2023-05-25 02:50:06 +1000

    Use GridStrategy for step sequencer

--- src/plugs/windows/channel_rack/sequence.py
+++ src/plugs/windows/channel_rack/sequence.py
@@ -9,15 +9,13 @@ Authors:
 This code is licensed under the GPL v3 license. Refer to the LICENSE file for
 more details.
 """
-
-from typing import Any
 import channels
 import ui
 from common.types.color import Color
 from common.plug_indexes import UnsafeIndex
+from common.util.grid_mapper import GridCell
 from control_surfaces import ControlShadowEvent
 from control_surfaces import (
-    DrumPad,
     DirectionLeft,
     DirectionRight,
     DirectionNext,
@@ -25,9 +23,11 @@ from control_surfaces import (
     DirectionUp,
     DirectionDown,
 )
+from control_surfaces.control_shadow import ControlShadow
 from devices import DeviceShadow
 from plugs import WindowPlugin
 from plugs.event_filters import filterButtonLift
+from plugs.mapping_strategies.grid_strategy import GridStrategy
 from .helpers import INDEX, getChannelRows
 
 # How many steps should be scrolled each time
@@ -42,19 +42,26 @@ class StepSequencer(WindowPlugin):
     """
 
     def __init__(self, shadow: DeviceShadow) -> None:
-        self._drums = \
-            shadow.bindMatches(DrumPad, self.drumPads)
-        # Number of rows and cols
-        self._height, self._width = shadow.getDevice().getDrumPadSize()
+        self._drums = GridStrategy(
+            16,
+            1,
+            self.triggerDrumPad,
+            color_callback=self.colorDrumPad,
+            # FIXME: Looks like horizontal_before_vertical is having,,, issues
+            # when set, indexes for wrapped rows are utterly incorrect
+            # horizontal_before_vertical=False,
+            wrap_overflows=True,
+        )
         # How far they scrolled
         self._scroll = 0
+
         self._left = shadow.bindMatch(DirectionLeft, self.left)
         self._right = shadow.bindMatch(DirectionRight, self.right)
         self._up = shadow.bindMatch(DirectionUp, self.up)
         self._down = shadow.bindMatch(DirectionDown, self.down)
         self._prev = shadow.bindMatch(DirectionPrevious, self.left)
         self._next = shadow.bindMatch(DirectionNext, self.right)
-        super().__init__(shadow, [])
+        super().__init__(shadow, [self._drums])
 
     @classmethod
     def getWindowId(cls) -> int:
@@ -64,34 +71,48 @@ class StepSequencer(WindowPlugin):
     def create(cls, shadow: DeviceShadow) -> 'WindowPlugin':
         return cls(shadow)
 
-    def drumPads(
+    def triggerDrumPad(
         self,
         control: ControlShadowEvent,
         idx: UnsafeIndex,
-        *args: Any
+        cell: GridCell,
     ) -> bool:
-        """Bind drum pads to step sequencer"""
-        # Ignore button lifts
+        """
+        Handle drum pad events
+        """
+        # For now, just ignore release events
+        # TODO: Later use them to implement graph editor features
         if not control.value:
-            return True
-        # Row should apply to the offset of the selected channel
-        # or if there aren't enough selections, the next selections along
-        selections = getChannelRows()
+            return False
+        channel = cell.group_number + getChannelRows()[0]
+        index = cell.group_index + self._scroll * SCROLL_MULTIPLIER
 
-        row, col = control.getControl().coordinate
-
-        # Implement scrolling
-        col += SCROLL_MULTIPLIER * self._scroll
-
-        # Check for index out of range
-        if row >= len(selections):
-            return True
-        # Get selected row
-        ch_idx = selections[row]
-
-        val = not channels.getGridBit(ch_idx, col)
-        channels.setGridBit(ch_idx, col, val)
+        val = channels.getGridBit(channel, index)
+        channels.setGridBit(channel, index, not val)
         return True
+
+    def colorDrumPad(
+        self,
+        control: ControlShadow,
+        idx: UnsafeIndex,
+        cell: GridCell,
+    ) -> Color:
+        """
+        Determine color for drum pads
+        """
+        channel = cell.group_number + getChannelRows()[0]
+        index = cell.group_index + self._scroll * SCROLL_MULTIPLIER
+
+        on_color = Color.fromGrayscale(1)
+
+        off_color = Color.fromInteger(
+            channels.getChannelColor(channel),
+            enabled=False
+        )
+        if channels.getGridBit(channel, index):
+            return on_color
+        else:
+            return off_color
 
     def showGrid(self) -> None:
         """Show the channel rack grid"""
@@ -100,11 +121,14 @@ class StepSequencer(WindowPlugin):
         # FIXME: This might get an index error on controllers with no drum pads
         row = getChannelRows()[0]
 
+        width = self._drums.get_group_size()
+        height = self._drums.get_num_groups_mapped()
+
         ui.crDisplayRect(
             col,
             row,
-            self._width,
-            self._height,
+            width,
+            height,
             1000,
         )
 
@@ -141,37 +165,3 @@ class StepSequencer(WindowPlugin):
         channels.selectOneChannel(new)
         self.showGrid()
         return True
-
-    def tick(self, *args):
-        """Set colors and annotations for omni-preview"""
-        # Row should apply to the offset of the selected channel
-        # or if there aren't enough selections, the next selections along
-        selections = getChannelRows()
-
-        for control in self._drums:
-            row, col = control.getControl().coordinate
-
-            if row >= len(selections):
-                control.color = DISABLED_COLOR
-                continue
-
-            ch_idx = selections[row]
-
-            # Implement scrolling
-            col += SCROLL_MULTIPLIER * self._scroll
-
-            # Determine color to use for when the grid bit isn't set
-            # if (col // SCROLL_MULTIPLIER) % 2:
-            #     false_color = BACK_BEAT_COLOR
-            # else:
-            #     false_color = LEAD_BEAT_COLOR
-            on_color = Color.fromGrayscale(1)
-
-            off_color = Color.fromInteger(
-                channels.getChannelColor(ch_idx),
-                enabled=False
-            )
-            if channels.getGridBit(ch_idx, col):
-                control.color = on_color
-            else:
-                control.color = off_color
