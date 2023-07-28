a17680adfd2c9e195d694c704a49bf53ed36c06d
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-29 01:32:19 +1000

    Refactor ActivitySwitcher plug

--- src/plugs/special/activity_switcher.py
+++ src/plugs/special/activity_switcher.py
@@ -14,16 +14,15 @@ Authors:
 This code is licensed under the GPL v3 license. Refer to the LICENSE file for
 more details.
 """
-import channels
-import mixer
-import ui
-import plugins
-
 from common.extension_manager import ExtensionManager
 from common import getContext
-from common.consts import WINDOW_NAMES
 from common.types import Color
-from common.plug_indexes import FlIndex, GeneratorIndex, WindowIndex
+from common.plug_indexes import (
+    FlIndex,
+    GeneratorIndex,
+    WindowIndex,
+    EffectIndex,
+)
 from control_surfaces import (
     ActivitySwitcher as ActivitySwitchControl,
     ControlShadowEvent,
@@ -55,60 +54,12 @@ def getActivityColor(activity: FlIndex) -> Color:
                 # Prevent issues if we deleted stuff
                 return Color.BLACK
         else:
+            assert isinstance(activity, EffectIndex)
             # Effect -> track color
             try:
-                return Color.fromInteger(mixer.getTrackColor(activity[0]))
+                return activity.track.color
             except TypeError:
                 return Color.BLACK
-
-
-def getActivityName(activity: SafeIndex) -> str:
-    """
-    Returns the name associated with a particular activity
-
-    ### Args:
-    * `activity` (`SafeIndex`): activity
-
-    ### Returns:
-    * `str`: name of activity
-    """
-    if isinstance(activity, int):
-        return WINDOW_NAMES[activity]
-    else:
-        if len(activity) == 1:
-            try:
-                return channels.getChannelName(*activity)
-            except TypeError:
-                # Prevent issues if we deleted stuff
-                return ""
-        else:
-            try:
-                return (
-                    # Mypy really should support tuple unpacking properly
-                    f"{mixer.getTrackName(activity[0])}: "  # type: ignore
-                    f"{plugins.getPluginName(*activity, True)}"
-                )
-            except TypeError:
-                # Prevent issues if we deleted stuff
-                return ""
-
-
-def triggerActivity(activity: SafeIndex):
-    """
-    Triggers the given activity
-
-    ### Args:
-    * `activity` (`SafeIndex`): activity to open
-    """
-    if isinstance(activity, int):
-        getContext().activity.ignoreNextHistory()
-        ui.showWindow(activity)
-    else:
-        if len(activity) == 1:
-            getContext().activity.ignoreNextHistory()
-            channels.focusEditor(activity[0])
-        else:
-            print("Would select effects")
 
 
 class ActivitySwitcher(SpecialPlugin):
@@ -138,7 +89,7 @@ class ActivitySwitcher(SpecialPlugin):
         c_index: int,
     ) -> bool:
         try:
-            triggerActivity(getContext().activity.getHistoryActivity(c_index))
+            getContext().activity.getHistoryActivity(c_index).focus()
         except IndexError:
             pass
         return True
@@ -152,7 +103,7 @@ class ActivitySwitcher(SpecialPlugin):
         try:
             activity = getContext().activity.getHistoryActivity(c_index)
             control.color = getActivityColor(activity)
-            control.annotation = getActivityName(activity)
+            control.annotation = activity.getName()
         except IndexError:
             pass
 
