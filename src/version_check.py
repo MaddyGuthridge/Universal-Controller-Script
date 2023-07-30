f6d28878b7408c1e98133de3d0f1b0dbab518669
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-31 02:02:50 +1000

    Make script load outside of FL Studio again

--- src/version_check.py
+++ src/version_check.py
@@ -2,14 +2,26 @@ from common.consts import checkFlVersion, MIN_FL_VERSION, formatVersion
 import ui
 
 
-if not checkFlVersion():
-    print()
-    print("CRITICAL ERROR!")
-    print("=" * 30)
-    print()
-    print(f"Your version of FL Studio ({ui.getVersion()}) is too low")
-    print(f"Please update to FL Studio >= {formatVersion(MIN_FL_VERSION)}")
-    print()
-    print()
+def versionCheck():
+    try:
+        # If we can import fl_model, that means we're running outside of FL
+        # Studio - don't block the version check
+        import fl_model
+        del fl_model
+        return
+    except ImportError:
+        pass
+    if not checkFlVersion():
+        print()
+        print("CRITICAL ERROR!")
+        print("=" * 30)
+        print()
+        print(f"Your version of FL Studio ({ui.getVersion()}) is too low")
+        print(f"Please update to FL Studio >= {formatVersion(MIN_FL_VERSION)}")
+        print()
+        print()
 
-    raise Exception("Script load cancelled")
+        raise Exception("Script load cancelled")
+
+
+versionCheck()
